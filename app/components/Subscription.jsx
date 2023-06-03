66a7e4313698b0ddad2c6648f23e572bbd27972f
Vented5 <venteddiagram@gmail.com>
2023-06-03 05:40:08 -0600

    Se agrego el componente de prising y cambios en la pagina de perfil

---
+++ app/components/Subscription.jsx
@@ -0,0 +1,57 @@
+
+export default function Subscription(){
+    return (
+        <>
+        <section className="flex space-x-12 justify-center lg:space-x-24 py-16">
+            <div className="flex flex-col border-4 border-white rounded-xl p-8 space-y-4">
+                <h2 className="text-2xl font-bold">Plan1</h2>
+                <p className="text-lg">This plan inculde basic benefits.</p>
+                <div className="flex items-end">
+                    <h1 className="text-5xl font-extrabold">$5</h1>
+                    <p>/month</p>
+                </div>
+                <hr/>
+                <button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy plan</button>
+                <li>
+                    <ul>5 products</ul>
+                    <ul>up to 10000 subscribers</ul>
+                    <ul>Basic adventages</ul>
+                    <ul>Increased access</ul>
+                </li>
+            </div>
+            <div className="flex flex-col border-4 border-white rounded-xl p-8 space-y-4">
+                <h2 className="text-2xl font-bold">Plan1</h2>
+                <p className="text-lg">This plan inculde basic benefits.</p>
+                <div className="flex items-end">
+                    <h1 className="text-5xl font-extrabold">$5</h1>
+                    <p>/month</p>
+                </div>
+                <hr/>
+                <button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy plan</button>
+                <li>
+                    <ul>5 products</ul>
+                    <ul>up to 10000 subscribers</ul>
+                    <ul>Basic adventages</ul>
+                    <ul>Increased access</ul>
+                </li>
+            </div>
+            <div className="flex flex-col border-4 border-white rounded-xl p-8 space-y-4">
+                <h2 className="text-2xl font-bold">Plan1</h2>
+                <p className="text-lg">This plan inculde basic benefits.</p>
+                <div className="flex items-end">
+                    <h1 className="text-5xl font-extrabold">$5</h1>
+                    <p>/month</p>
+                </div>
+                <hr/>
+                <button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buy plan</button>
+                <li>
+                    <ul>5 products</ul>
+                    <ul>up to 10000 subscribers</ul>
+                    <ul>Basic adventages</ul>
+                    <ul>Increased access</ul>
+                </li>
+            </div>
+        </section>
+        </>
+    )
+}
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,20 @@
+import Subscription from '../components/Subscription';
 
 export default function Profile() {
     return (
-      <h1>Profile</h1>
-    )
+      <>
+        <section className='flex pl-24'>
+            
+            <img src='#'className='h-64 w-64 rounded-full bg-white'></img>
+            <div className='flex-col'>
+                <h1 className='text-6xl'>Zanero</h1>
+                <p>Code master... coffee is never enough</p>
+            </div>
+          
+        </section>
+        <Subscription />
+    
+        </>
+      )
   }
   
